f5d917a789c268158bbbdb13b89fd14c02791532
AidaMart <aida.martirosyan2002@gmail.com>
2023-12-05 00:09:05 +0400

    adjusting CLV calculation to AFT

--- clv/models/CLV.py
+++ clv/models/CLV.py
@@ -1,59 +1,45 @@
-from .Data_Processing import *
+from .Data_Processing import MM, p
 
 class CLV():
-    """
-    Customer Lifetime Value (CLV) calculator using survival analysis and discounting.
-
-    Parameters:
-    -----------
-    t : int
-        Time for which CLV is calculated.
-
-    Attributes:
-    -----------
-    t : int
-        Time for which CLV is calculated.
-
-    Methods:
-    --------
-    calculate_clv():
-        Calculate the Customer Lifetime Value (CLV) based on survival probabilities and discounting.
-    """
-    def __init__(self, t):
+    def __init__(self, t, customer_id):
         """
-        Initialize the CLV calculator with the specified time 't'.
+        Initialize the CLV calculator with the specified time 't' and customer ID.
 
         Parameters:
         -----------
         t : int
             Time for which CLV is calculated.
+        customer_id : int
+            Identifier for the customer, for which the CLV is calculated.
         """
         self.t = t
+        self.customer_id = customer_id
 
     def calculate_clv(self):
         """
-        Calculate the Customer Lifetime Value (CLV) based on survival probabilities and discounting.
+        Calculate the Customer Lifetime Value (CLV) based on survival probabilities.
 
         Returns:
         --------
         float:
-            The calculated Customer Lifetime Value (CLV).
+            The calculated Customer Lifetime Value (CLV) rounded to three decimal places.
         """
-
         # Define the discount rate
         discount_rate = 0.1025  # 10.25% as a decimal
 
         # Get the monthly margins and probabilities for the customer
         monthly_margin = MM()
-        customer_probabilities = p(self.t)
+        customer_probabilities = p()
+
+        # Extract relevant probabilities based on t and customer_id
+        probabilities_list = customer_probabilities.loc[1:self.t, self.customer_id].values.tolist()
 
         # Calculate the CLV using the formula
         clv = 0
-        for i in range(len(customer_probabilities)):
-            clv += (customer_probabilities[i] / ((1 + discount_rate / 12) ** i))
+        for i in range(len(probabilities_list)):
+            clv += (probabilities_list[i] / ((1 + discount_rate / 12) ** i))
 
         clv *= monthly_margin
+        clv = round(clv, 3)
 
         return clv
-
-
