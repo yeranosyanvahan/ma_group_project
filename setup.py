15713ef6955178d8a2d1d6733f1e444af6e479c4
Vahan Yeranosyan <yeranosyanvahan@gmail.com>
2023-12-06 00:41:44 +0400

    delete dist and update gitignore

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+# Read the contents of your README file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     author='Group 5',
+    packages=find_packages(include=['clv', 'clv.*']),
     url='https://github.com/yeranosyanvahan/ma_group_project',
     description='CLV prediction',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Specify the format
     name='SocraCLV',
     version='0.9.9',
-    packages=find_packages(include=['clv','clv.*']),
 )
